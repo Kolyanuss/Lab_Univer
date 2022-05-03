95cce59084216748b59749c0ec501932820096d7
0xKolyanus <mukola.1zt.@gmail.com>
2022-05-03 15:55:27 +0300

    fix EDA lab 4

--- EDA/lab4/main.py
+++ EDA/lab4/main.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score, confusion_matrix
-from sklearn.preprocessing import LabelEncoder
 from sklearn.svm import SVC
 from sklearn.model_selection import GridSearchCV, train_test_split, StratifiedKFold
 
@@ -14,7 +13,7 @@ y = df.values[:, 0]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=100)
 
 
-def myPredict(kernel='linear', c=1, gamma='scale'):
+def Predict(kernel='linear', c=1, gamma='scale'):
     clf = SVC(kernel=kernel, C=c, gamma=gamma)
     clf1 = clf.fit(X_train, y_train)
 
@@ -24,32 +23,9 @@ def myPredict(kernel='linear', c=1, gamma='scale'):
     print(confusion_matrix(y_test, y_pred))
 
 
-# myPredict()
+# -----
 
-
-# plt.scatter(X[:, 0], X[:, 1], c=y, s=30, cmap=plt.cm.Paired)
-#
-# ax = plt.gca()
-# xlim = ax.get_xlim()
-# ylim = ax.get_ylim()
-#
-# xx = np.linspace(xlim[0], xlim[1], 30)
-# yy = np.linspace(ylim[0], ylim[1], 30)
-# YY, XX = np.meshgrid(yy, xx)
-# xy = np.vstack([XX.ravel(), YY.ravel()]).T
-# Z = clf.decision_function(xy).reshape(XX.shape)
-#
-# ax.contour(XX, YY, Z, colors='k', levels=[-1, 0, 1],
-#            alpha=0.5, linestyles=['--', '-', '--'])
-# ax.scatter(clf.support_vectors_[:, 0], clf.support_vectors_[:, 1],
-#            s=100, linewidth=1, facecolors='none')
-# plt.show()
-
-
-# # -----
-
-def plotSVC(title):
-    global svc
+def plotSVC(title, svc):
     # create a mesh to plot in
     x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
@@ -69,41 +45,15 @@ def plotSVC(title):
     pass
 
 
-#
-# kernels = ['linear', 'rbf', 'poly']
-# for kernel in kernels:
-#     svc = SVC(kernel=kernel).fit(X_train, y_train)
-#     plotSVC('kernel = ' + str(kernel))
-#
-# gammas = [0.1, 1, 10, 100]
-# for gamma in gammas:
-#     svc = SVC(kernel='rbf', gamma=gamma).fit(X, y)
-#     plotSVC('kernel = rbf, gamma(' + str(gamma) + ')')
-#
-# cs = [0.1, 1, 10, 100, 1000]
-# for c in cs:
-#     svc = SVC(kernel='rbf', C=c).fit(X, y)
-#     plotSVC('kernel = rbf, C(' + str(c) + ')')
-#
-# degrees = [0, 1, 2, 3, 4, 5, 6]
-# for degree in degrees:
-#     svc = SVC(kernel='poly', degree=degree).fit(X, y)
-#     plotSVC('kernel = poly, degree(' + str(degree) + ')')
-
-
 def svc_param_selection():
-    kernels = ['linear', 'rbf', 'poly']
-    Cs = [0.001, 0.01, 0.1, 1, 10]
-    gammas = [0.001, 0.01, 0.1, 1]
-    param_grid = {'kernel': kernels, 'C': Cs, 'gamma': gammas}
-    param_grid2 = [{'kernel': ['rbf'],
-                    'C': [0.001, 0.01, 0.1, 1, 10, 100],
-                    'gamma': [0.001, 0.01, 0.1, 1, 10, 100]},
-                   {'kernel': ['linear'],
-                    'C': [0.001, 0.01, 0.1, 1, 10, 100]}]
+    param_grid = [{'kernel': ['rbf'],
+                   'C': [0.001, 0.01, 0.1, 1, 10, 100],
+                   'gamma': [0.001, 0.01, 0.1, 1, 10, 100]},
+                  {'kernel': ['linear'],
+                   'C': [0.001, 0.01, 0.1, 1, 10, 100]}]
 
     kfold = StratifiedKFold(n_splits=3, shuffle=True, random_state=100)
-    grid_search = GridSearchCV(SVC(), param_grid2, scoring="accuracy", cv=kfold, verbose=1)
+    grid_search = GridSearchCV(SVC(), param_grid, scoring="accuracy", cv=kfold, verbose=1)
     grid_result = grid_search.fit(X_train, y_train)
     svc = grid_result.best_params_
     # summarize results
@@ -112,7 +62,12 @@ def svc_param_selection():
     return svc
 
 
-rez = svc_param_selection()
-myPredict(rez["kernel"], rez["C"]) # to do: add gamma
+Predict()  # запуск ф-ї із дефолтними параметрами
 
-# plotSVC(str(rez))
+rez = svc_param_selection()  # пошук найкращих параметрів
+
+if rez.get('gamma') != None:
+    Predict(rez.get("kernel"), rez.get("C"), rez.get("gamma"))  # підставлення найкращого параметру у функцію
+else:
+    Predict(rez.get("kernel"), rez.get("C"))  # підставлення найкращого параметру у функцію
+
