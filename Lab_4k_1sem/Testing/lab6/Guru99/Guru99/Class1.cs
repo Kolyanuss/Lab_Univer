326b068c68ab3749f32f14d99cceb35e390776b2
0xKolyanus <mukola.1zt.@gmail.com>
2022-09-08 10:43:38 +0300

    add Testing lab

---
+++ Lab_4k_1sem/Testing/lab6/Guru99/Guru99/Class1.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+namespace Guru99
+{
+    class Class1
+    {
+        IWebDriver driver;
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver("E:\\___Code___\\__Programing__\\Lab_University\\Lab_4k_1sem\\Testing\\lab6\\Guru99");
+        }
+
+        [Test]
+        public void test()
+        {
+            driver.Url = "https://uk.wikipedia.org/wiki";
+
+            Assert.AreEqual("Вікіпедія", driver.Title.ToString());
+
+            var element = driver.FindElement(By.Id("n-mainpage-description"));
+            Assert.AreEqual("Головна сторінка", element.Text);
+            element.Click();
+
+            element = driver.FindElement(By.Id("n-currentevents"));
+            Assert.AreEqual("Поточні події", element.Text);                        
+        }
+
+        [Test]
+        public void test2()
+        {
+            driver.Url = "https://uk.wikipedia.org/wiki";
+
+            Assert.AreEqual("Вікіпедія", driver.Title.ToString());
+
+            var element = driver.FindElement(By.Id("feat-article"));
+            Assert.AreEqual("rgba(250, 250, 250, 1)", element.GetCssValue("background-color"));                     
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
