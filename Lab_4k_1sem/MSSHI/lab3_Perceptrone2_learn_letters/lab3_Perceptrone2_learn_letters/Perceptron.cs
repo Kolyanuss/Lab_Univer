fc3fb064bdc0d9bbdd2f9a931200d7d2e219fbe4
0xKolyanus <mukola.1zt.@gmail.com>
2022-09-25 15:39:54 +0300

    modified the neuron to increase flexibility by size

--- Lab_4k_1sem/MSSHI/lab3_Perceptrone2_learn_letters/lab3_Perceptrone2_learn_letters/Perceptron.cs
+++ Lab_4k_1sem/MSSHI/lab3_Perceptrone2_learn_letters/lab3_Perceptrone2_learn_letters/Perceptron.cs
@@ -7,7 +7,18 @@ namespace lab3_Perceptrone2_learn_letters
     public class Perceptron
     {
         private Neiron[] neirons;
-        public int numOfLetter = 33;
+        public const int numOfLetter = 33;
+        /// <summary>
+        /// How many parts will the picture be divided into on the X axis.
+        /// На скільки частин буде поділятись картинка по осі Х
+        /// </summary>
+        public int sizeX { get; } = 6;
+        /// <summary>
+        /// How many parts will the picture be divided into on the Y axis.
+        /// На скільки частин буде поділятись картинка по осі Y
+        /// </summary>
+        public int sizeY { get; } = 8;
+        public int generalSize { get { return sizeX * sizeY; } }
 
         public Perceptron()
         {
@@ -18,13 +29,30 @@ namespace lab3_Perceptrone2_learn_letters
 
             for (int i = 0; i < neirons.Length - 7; i++)
             {
-                //Console.WriteLine(A_letter);
                 neirons[i] = new Neiron(A_letter++);
             }
             for (int i = 0; i < additionalVal.Length; i++)
             {
-                //Console.WriteLine(additionalVal[i]);
                 neirons[neirons.Length - 7 + i] = new Neiron(additionalVal[i]);
+            }
+        }
+
+        public Perceptron(int sizeX, int sizeY)
+        {
+            this.sizeX = sizeX;
+            this.sizeY = sizeY;
+            neirons = new Neiron[numOfLetter];
+
+            char A_letter = Convert.ToChar(1040);
+            char[] additionalVal = new char[7] { 'Ґ', 'Є', 'І', 'Ї', 'Ь', 'Ю', 'Я' };
+
+            for (int i = 0; i < neirons.Length - 7; i++)
+            {
+                neirons[i] = new Neiron(A_letter++, generalSize);
+            }
+            for (int i = 0; i < additionalVal.Length; i++)
+            {
+                neirons[neirons.Length - 7 + i] = new Neiron(additionalVal[i], generalSize);
             }
         }
 
