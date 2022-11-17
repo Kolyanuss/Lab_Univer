181d8a03d047f13f95325f97359862d77042650f
0xKolyanus <mukola.1zt.@gmail.com>
2022-11-17 17:22:32 +0200

    started create genetic algorithm

---
+++ Lab_4k_1sem/MSSHI/lab11_GeneticsAlgorithm/Logic/Core.cs
@@ -0,0 +1,19 @@
+﻿namespace Logic
+{
+    public class Core
+    {
+        public int chromosomeLenght = 20;
+        public int populationSize = 100;
+        public double probabilityCrossover = 1;
+        public double probabilityMutation = 0.1;
+        public int CountMaxGeneration = 50;
+
+        Func<int> fitnesFunc;
+
+        public Core()
+        {
+
+        }
+
+    }
+}
