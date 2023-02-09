ea68dfd44ad65938503c00f35e58d8bd8bd7bd12
0xKolyanus <mukola.1zt.@gmail.com>
2023-02-09 16:51:15 +0200

    1st lb skbd

---
+++ Lab_4k_2sem/SKBD/lb1/sql.php
@@ -0,0 +1,35 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf8">
+<?php
+// Змінні з форми
+$pib = $_POST['pib'];
+$tel = $_POST['tel'];
+$com = $_POST['com'];
+// Параметри для підключення
+$db_host = "sql212.epizy.com";
+$db_user = "epiz_33558799"; // Логін БД
+$db_password = "r7mVyAVJMIsvcf"; // Пароль БД
+$db_base = 'epiz_33558799_zakaz'; // Назва БД
+$db_table = "zakaz"; // Назва таблиці БД
+// Підключення до бази даних
+$mysqli = new mysqli($db_host, $db_user, $db_password, $db_base);
+// Якщо є помилка підключення, виводимо її і ліквідуємо підключення
+if ($mysqli->connect_error) {
+    die('Помилка: (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+}
+// Виконуємо запит
+$result = $mysqli->query("INSERT INTO " . $db_table . " (pib,tel, com) VALUES ('$pib','$tel','$com')");
+// Перевіряємо виконання запиту та виводимо відповідне повідомлення
+if ($result == true) {
+    echo '<center>Інформація занесена в базу даних!</br><a href="index.html">Повернутись на головну</a></center>';
+    $sql_select = "SELECT * FROM zakaz";
+    $result2 = mysqli_query($mysqli, $sql_select);
+    $row = mysqli_fetch_array($result2);
+    do {
+        printf("<p>ПІБ: " . $row['pib'] . "</p><p>Номер телефону: " . $row['tel'] . "</p>
+        <p>Коментар: " . $row['com'] . "</p></br>");
+    } while ($row = mysqli_fetch_array($result2));
+} else {
+    echo 'Інформація не занесена в базу даних';
+}
+mysqli_close($mysqli);
+?>
