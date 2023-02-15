4a04ac8f56cd0e030dc1182be4f2c21debd17928
0xKolyanus <mukola.1zt.@gmail.com>
2023-02-15 21:18:50 +0200

    update geo lab

--- Lab_4k_2sem/Geo Information Systems/lab1/main.py
+++ Lab_4k_2sem/Geo Information Systems/lab1/main.py
@@ -1,6 +1,6 @@
 import osgeo.ogr
 shapefile = osgeo.ogr.Open("poland_airport_polygon.shp")
-numLayers = shapefile. GetLayerCount()
+numLayers = shapefile.GetLayerCount()
 print("Файл фігур містить {} шарів".format(numLayers))
 print()
 for layerNum in range(numLayers):
@@ -9,8 +9,8 @@ for layerNum in range(numLayers):
     numFeatures = layer.GetFeatureCount()
     print("шар {} має просторову прив’язку {}".format(layerNum, spatialRef))
     print("шар {} містить {} геооб’єктів: ".format(layerNum, numFeatures))
-print()
-for featureNum in range(numFeatures):
-    feature = layer.GetFeature(featureNum)
-    featureName = feature.GetField("NAME")
-    print("Геооб’єкт {} під назвою {}". format(featureNum, featureName))
+    print()
+    for featureNum in range(numFeatures):
+        feature = layer.GetFeature(featureNum)
+        featureName = feature.GetField("NAME")
+        print("Геооб’єкт {} під назвою {}". format(featureNum, featureName))
